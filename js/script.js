3ab113dd8877558ce9a1c9464e72aa94a6086e6e
Rosamund Pearce <rosamundpearce@live.com>
2018-03-20 13:52:22 +0000

    both filters working

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoicm9zcGVhcmNlIiwiYSI6ImNpdm1sczJsZjAwOGMyeW1xNHc4ejJ0N28ifQ.4B24e0_HgfJj4sgqimETqA';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2lqbmpqazdlMDBsdnRva284cWd3bm11byJ9.V6Hg2oYJwMAxeoR9GEzkAA';
 
 var map = new mapboxgl.Map({
-  container: 'map', // container element id
-  style: 'mapbox://styles/mapbox/light-v9',
-  center: [-74.0059, 40.7128], // initial map center in [lon, lat]
-  zoom: 12
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-74.0059, 40.7128],
+    zoom: 11
 });
 
 map.on('load', function() {
+
+    var filterHour = ['==', ['number',['get', 'Hour']], 12];
+    var filterDay = ['!=', ['string',['get', 'Day']], 'placeholder'];
+
     map.addLayer({
       id: 'collisions',
       type: 'circle',
       source: {
         type: 'geojson',
-        data: './data/collisions1601.geojson' // replace this with the url of your own geojson
+        data: './data/collisions1601.geojson'
       },
       paint: {
         'circle-radius': [
@@ -36,19 +40,38 @@ map.on('load', function() {
         ],
         'circle-opacity': 0.8
       },
-      filter: ['==', ['number', ['get', 'Hour']], 12]
+      'filter': ['all', filterHour, filterDay]
     }, 'admin-2-boundaries-dispute');
+
+    // update hour filter when the slider is dragged
+    document.getElementById('slider').addEventListener('input', function(e) {
+      var hour = parseInt(e.target.value);
+      // update the map
+      filterHour = ['==', ['number', ['get', 'Hour']], hour];
+      map.setFilter('collisions', ['all', filterHour, filterDay]);
+
+      // converting 0-23 hour to AMPM format
+      var ampm = hour >= 12 ? 'PM' : 'AM';
+      var hour12 = hour % 12 ? hour % 12 : 12;
+
+      // update text in the UI
+      document.getElementById('active-hour').innerText = hour12 + ampm;
+    });
+
+    document.getElementById('filters').addEventListener('change', function(e) {
+      var day = e.target.value;
+      // update the map filter
+      if (day === 'all') {
+        filterDay = ['!=', ['string', ['get','Day']], 'placeholder'];
+      } else if (day === 'weekday') {
+        filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], false, true];
+      } else if (day === 'weekend') {
+        filterDay = ['match', ['get', 'Day'], ['Sat', 'Sun'], true, false];
+      } else {
+        console.log('error');
+      };
+      map.setFilter('collisions', ['all', filterHour, filterDay]);
+    });
+
 });
-
-document.getElementById('slider').addEventListener('input', function(e) {
-    var hour = parseInt(e.target.value);
-    // update the map
-    map.setFilter('collisions', ['==', ['number', ['get', 'Hour']], hour]);
-
-    // converting 0-23 hour to AMPM format
-    var ampm = hour >= 12 ? 'PM' : 'AM';
-    var hour12 = hour % 12 ? hour % 12 : 12;
-
-    // update text in the UI
-    document.getElementById('active-hour').innerText = hour12 + ampm;
-});
+  
