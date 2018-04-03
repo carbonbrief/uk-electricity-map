8011c96b54963fad8a3895937f03b5a012d7d727
Rosamund Pearce <rosamundpearce@live.com>
2018-04-03 11:15:00 +0100

    switch names to plant

--- js/line-chart-2.js
+++ js/line-chart-2.js
@@ -55,6 +55,7 @@ d3.csv("./data/dummy.csv", function(error, data) {
     d3.max(powerplants, function(c) { return d3.max(c.values, function(v) { return v.capacity; }); })
   ]);
   svg.selectAll("*").remove();
+
   //LEGEND
   var legend = svg.selectAll('g')
       .data(powerplants)
@@ -104,34 +105,34 @@ d3.csv("./data/dummy.csv", function(error, data) {
   console.log("filter");
   console.log(boo);
   
-  var city = svg.selectAll(".city")
+  var plant = svg.selectAll(".plant")
       .data(powerplants.filter(function(d){return filterData[d.name]==true;})) //.filter(function(d){return filterData[d.name]==true;})
       .enter().append("g");
-    //  .attr("class", "city");
+    //  .attr("class", "plant");
       
-     console.log(city);  
-      svg.selectAll(".city")
+     console.log(plant);  
+      svg.selectAll(".plant")
       .data(powerplants.filter(function(d){return filterData[d.name]==true;}))//.filter(function(d){return filterData[d.name]==true;})
       .append("g")
-      .attr("class", "city");
+      .attr("class", "plant");
       
-      svg.selectAll(".city")
+      svg.selectAll(".plant")
       .data(powerplants.filter(function(d){return filterData[d.name]==true;}))
       .exit()
       .remove();
   
-  city.append("path")
+  plant.append("path")
       .attr("class", "line")
       .attr("d", function(d) { return line(d.values); })
       .style("stroke", function(d) { return color(d.name); });
 
-  city.append("text")
+  plant.append("text")
       .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
       .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.capacity) + ")"; })
       .attr("x", 3)
       .attr("dy", ".35em")
       .text(function(d) { return d.name; });
-    svg.selectAll(".city")
+    svg.selectAll(".plant")
       .data(powerplants.filter(function(d){return filterData[d.name]==true;}))
       .exit()
       .remove();
