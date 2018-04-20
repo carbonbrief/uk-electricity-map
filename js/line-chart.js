09b9254f003c4a71fe351cdcef64b40ce90a4a8f
Rosamund Pearce <rosamundpearce@live.com>
2018-04-20 15:10:47 +0100

    line that moves along years working

--- js/line-chart.js
+++ js/line-chart.js
@@ -46,7 +46,10 @@ d3.csv("./data/line.csv", function(error, data) {
     return {
       name: name,
       values: data.map(function(d) {
-        return {year: d.year, capacity: +d[name]};
+        return {
+            year: d.year, 
+            capacity: +d[name]
+        };
       })
     };
   });
@@ -65,7 +68,7 @@ d3.csv("./data/line.csv", function(error, data) {
 
 
 
-    // link behaviour to dropdown
+    // LINK BEHAVIOUR TO DROPDOWN
 
     d3.select("#selectorType").on("change", selectType)
 
@@ -140,97 +143,136 @@ d3.csv("./data/line.csv", function(error, data) {
       .exit()
       .remove();
 
-    // MOUSE EFFECTS
+    // MOUSE EFFECTS 1
 
-    var mouseG = svg.append("g")
-    .attr("class", "mouse-over-effects");
+    // var mouseG = svg.append("g")
+    // .attr("class", "mouse-over-effects");
 
-    mouseG.append("path") // this is the black vertical line to follow mouse
-    .attr("class", "mouse-line")
-    .style("stroke", "gray")
-    .style("stroke-width", "1px")
-    .style("opacity", "0");
+    // mouseG.append("path") // this is the black vertical line to follow mouse
+    // .attr("class", "mouse-line")
+    // .style("stroke", "gray")
+    // .style("stroke-width", "1px")
+    // .style("opacity", "0");
 
-    var lines = document.getElementsByClassName('line');
+    // var lines = document.getElementsByClassName('line');
 
-    var mousePerLine = mouseG.selectAll('.mouse-per-line')
-    .data(powerplants)
-    .enter()
-    .append("g")
-    .attr("class", "mouse-per-line");
+    // var mousePerLine = mouseG.selectAll('.mouse-per-line')
+    // .data(powerplants)
+    // .enter()
+    // .append("g")
+    // .attr("class", "mouse-per-line");
 
-    mousePerLine.append("circle")
-        .attr("r", 5)
-        .style("stroke", function(d) {
-        return color(d.name);
-        })
-        .style("fill", "none")
-        .style("stroke-width", "1px")
-        .style("opacity", "0");
+    // mousePerLine.append("circle")
+    //     .attr("r", 5)
+    //     .style("stroke", function(d) {
+    //     return color(d.name);
+    //     })
+    //     .style("fill", "none")
+    //     .style("stroke-width", "1px")
+    //     .style("opacity", "0");
 
-    mousePerLine.append("text")
-        .attr("transform", "translate(10,3)");
+    // mousePerLine.append("text")
+    //     .attr("transform", "translate(10,3)");
 
-    mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
-        .attr('width', width) // can't catch mouse events on a g element
-        .attr('height', height)
-        .attr('fill', 'none')
-        .attr('pointer-events', 'all')
-        .on('mouseout', function() { // on mouse out hide line, circles and text
-        d3.select(".mouse-line")
-            .style("opacity", "0");
-        d3.selectAll(".mouse-per-line circle")
-            .style("opacity", "0");
-        d3.selectAll(".mouse-per-line text")
-            .style("opacity", "0");
-        })
-        .on('mouseover', function() { // on mouse in show line, circles and text
-        d3.select(".mouse-line")
-            .style("opacity", "1");
-        d3.selectAll(".mouse-per-line circle")
-            .style("opacity", "1");
-        d3.selectAll(".mouse-per-line text")
-            .style("opacity", "1");
-        })
-        .on('mousemove', function() { // mouse moving over canvas
+    // mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
+    //     .attr('width', width) // can't catch mouse events on a g element
+    //     .attr('height', height)
+    //     .attr('fill', 'none')
+    //     .attr('pointer-events', 'all')
+    //     .on('mouseout', function() { // on mouse out hide line, circles and text
+    //     d3.select(".mouse-line")
+    //         .style("opacity", "0");
+    //     d3.selectAll(".mouse-per-line circle")
+    //         .style("opacity", "0");
+    //     d3.selectAll(".mouse-per-line text")
+    //         .style("opacity", "0");
+    //     })
+    //     .on('mouseover', function() { // on mouse in show line, circles and text
+    //     d3.select(".mouse-line")
+    //         .style("opacity", "1");
+    //     d3.selectAll(".mouse-per-line circle")
+    //         .style("opacity", "1");
+    //     d3.selectAll(".mouse-per-line text")
+    //         .style("opacity", "1");
+    //     })
+    //     .on('mousemove', function() { // mouse moving over canvas
 
-    var mouse = d3.mouse(this);
+    // var mouse = d3.mouse(this);
 
-    d3.select(".mouse-line")
-        .attr("d", function() {
-        var d = "M" + mouse[0] + "," + height;
-        d += " " + mouse[0] + "," + 0;
-        return d;
-        });
+    // d3.select(".mouse-line")
+    //     .attr("d", function() {
+    //     var d = "M" + mouse[0] + "," + height;
+    //     d += " " + mouse[0] + "," + 0;
+    //     return d;
+    //     });
 
-    d3.selectAll(".mouse-per-line")
-        .attr("transform", function(d, i) {
-        //console.log(width/mouse[0])
-        var xDate = x.invert(mouse[0]),
-            bisect = d3.bisector(function(d) { return d.year; }).right;
-            idx = bisect(d.values, xDate);
+    // d3.selectAll(".mouse-per-line")
+    //     .attr("transform", function(d, i) {
+    //     //console.log(width/mouse[0])
+    //     var xDate = x.invert(mouse[0]);
         
-        var beginning = 0,
-            end = lines[i].getTotalLength(),
-            target = null;
+    //     var beginning = 0,
+    //         end = lines[i].getTotalLength(),
+    //         target = null;
 
-        while (true){
-            target = Math.floor((beginning + end) / 2);
-            pos = lines[i].getPointAtLength(target);
-            if ((target === end || target === beginning) && pos.x !== mouse[0]) {
-                break;
-            }
-            if (pos.x > mouse[0])      end = target;
-            else if (pos.x < mouse[0]) beginning = target;
-            else break; //position found
-        }
+    //     while (true){
+    //         target = Math.floor((beginning + end) / 2);
+    //         pos = lines[i].getPointAtLength(target);
+    //         if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+    //             break;
+    //         }
+    //         if (pos.x > mouse[0])      end = target;
+    //         else if (pos.x < mouse[0]) beginning = target;
+    //         else break; //position found
+    //     }
         
-        d3.select(this).select('text')
-            .text(y.invert(pos.y).toFixed(0));
+    //     d3.select(this).select('text')
+    //         .text(y.invert(pos.y).toFixed(0));
             
-        return "translate(" + mouse[0] + "," + pos.y +")";
-        });
-    });
+    //     return "translate(" + mouse[0] + "," + pos.y +")";
+    //     });
+    // });
+
+    // MOUSE EFFECTS 2
+
+    var focus = svg.append('g')
+                .attr('class', 'focus')
+                .style('display', 'none');
+
+    focus.append('line')
+    .attr('class', 'x-hover-line hover-line')
+    .attr('y1' , 0)
+    .attr('y2', height);
+
+    svg.append('rect')
+        //.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "overlay")
+        .attr("width", width)
+        .attr("height", height)
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout)
+        .on("mousemove", mousemove);
+
+    var timeScales = data.map(function(d) { return x(d.year); });
+
+    function mouseover() {
+        focus.style("display", null);
+        d3.selectAll('.points text').style("display", null);
+    }
+    function mouseout() {
+        focus.style("display", "none");
+        d3.selectAll('.points text').style("display", "none");
+    }
+    function mousemove() {
+        var i = d3.bisect(timeScales, d3.mouse(this)[0], 1);
+        var di = data[i-1];
+        focus.attr("transform", "translate(" + x(di.year) + ",0)");
+        d3.selectAll('.points text')
+        .attr('x', function(d) { return x(di.year) + 15; })
+        .attr('y', function(d) { return y(d.capacity[i-1].total); })
+        .text(function(d) { return d.values[i-1].total; });
+        //.style('fill', function(d) { return z(d.name); });
+    }
 
 });
 
