652826a345009bcfb77a3cfcc2028c09766a1aa2
Rosamund Pearce <rosamundpearce@live.com>
2018-05-04 13:22:34 +0100

    Filtered stacked bar

--- js/stacked-bar.js
+++ js/stacked-bar.js
@@ -1,11 +1,17 @@
-// with help from https://bl.ocks.org/Andrew-Reid/0aedd5f3fb8b099e3e10690bd38bd458
+// with help from https://bl.ocks.org/reinson/166bae46dd106b45cf2d77c7802768ca
 
 var margin = {top: 20, right: 70, bottom: 30, left: 40},
     width = 450 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
 
-var svg3 = d3.select('#stacked-bar').append("svg"),
-    g = svg3.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg3 = d3.select('#stacked-bar').append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+g = svg3.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// have to create different names as using other similar variables in the same document
 
 var y3 = d3.scaleBand()			
     .rangeRound([0, height])	
@@ -16,82 +22,78 @@ var x3 = d3.scaleLinear()
     .rangeRound([0, width]);	
 
 var z3 = d3.scaleOrdinal()
-    .range(["#ced1cc", "#00a98e"]);
+    .range(["#00a98e", "#ced1cc"]);
 
-// year to be shown
-var getYear = 2017;
+var stack = d3.stack();
 
-d3.csv("./data/stacked-bar.csv", function(d, i, columns) {
+d3.csv("./data/stacked-bar.csv", function(error, data) {
+  if (error) throw error;
 
-    for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-    d.total = t;
-    return d;
+  var data_nest = d3.nest()
+        .key(function(d){
+            return d.year
+        })
+        .entries(data);
 
-    filteredData = data.filter(function(row) {
-        return row['year'] == getYear
-    })
-    
-  }, function(error, data) {
-    if (error) throw error;
+    console.log(data_nest);
+
+  data = data_nest.filter(function(d){ return d.key == 2017})[0].values;
+
+  console.log(data);
   
-    var keys = data.columns.slice(1);
-  
-    data.sort(function(a, b) { return b.total - a.total; });
-    y3.domain(data.map(function(d) { return d.year; }));					// x.domain...
-    x3.domain([0, d3.max(data, function(d) { return d.total; })]).nice();	// y.domain...
-    z3.domain(keys);
-  
-    g.append("g")
-      .selectAll("g")
-      .data(d3.stack().keys(keys)(data))
-      .enter().append("g")
-        .attr("fill", function(d) { return z3(d.key); })
+  var cat = ["lowCarbon","highCarbon"];
+
+  y3.domain(data.map(function(d) { return d.y; }));
+  x3.domain([0, 100]).nice();
+  z3.domain(cat); 
+
+  g.selectAll(".serie")
+    .data(stack.keys(cat)(data))
+    .enter().append("g")
+      .attr("class", "serie")
+      .attr("fill", function(d) {return z3(d.key); })
+    .selectAll("rect")
+    .data(function(d) { return d; })
+    .enter().append("rect")
+      .attr("y", function(d) { return y3(d.data.y); })
+      .attr("x", function(d) { return x3(d[0]); })
+      .attr("width", function(d) { return x3(d[1]) - x3(d[0]); })
+      .attr("height", y3.bandwidth());
+
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height/2 + ")")
+      .style("font", "18px sans-serif")
+      .call(d3.axisLeft(y3));
+
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .attr("transform","translate(0," + height + ")")
+      .call(d3.axisBottom(x3).ticks(5, "s"))
+    .append("text")
+      .attr("y", 2)
+      .attr("x", x3(x3.ticks(10).pop()))
+      .attr("dy", "0.35em")
+      .attr("text-anchor", "start")
+      .attr("fill", "#000");
+
+  d3.select("input")
+    .on("input", changed)
+    .on("change", changed);
+
+  function changed() {
+    var value = this.value;
+
+    g.selectAll(".serie")
+      .data(stack.keys(cat)(data_nest.filter(function(d){return +d.key == value})[0].values))
       .selectAll("rect")
       .data(function(d) { return d; })
-      .enter().append("rect")
-        .attr("y", function(d) { return y3(d.data.year); })	    //.attr("x", function(d) { return x(d.data.year); })
-        .attr("x", function(d) { return x3(d[0]); })			    //.attr("y", function(d) { return y(d[1]); })	
-        .attr("width", function(d) { return x3(d[1]) - x3(d[0]); })	//.attr("height", function(d) { return y(d[0]) - y(d[1]); })
-        .attr("height", y3.bandwidth());						    //.attr("width", x.bandwidth());	
-  
-    g.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0,0)") 						//  .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisLeft(y3));									//   .call(d3.axisBottom(x));
-  
-    g.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0,"+height+")")				// New line
-        .call(d3.axisBottom(x3).ticks(null, "s"))					//  .call(d3.axisLeft(y).ticks(null, "s"))
-      .append("text")
-        .attr("y", 2)												//     .attr("y", 2)
-        .attr("x", x3(x3.ticks().pop()) + 0.5) 						//     .attr("y", y(y.ticks().pop()) + 0.5)
-        .attr("dy", "0.32em")										//     .attr("dy", "0.32em")
-        .attr("fill", "#000")
-        .attr("font-weight", "bold")
-        .attr("text-anchor", "start")
-        .text("Population")
-        .attr("transform", "translate("+ (-width) +",-10)");   	// Newline
-  
-    var legend = g.append("g")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10)
-        .attr("text-anchor", "end")
-      .selectAll("g")
-      .data(keys.slice().reverse())
-      .enter().append("g")
-      //.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-       .attr("transform", function(d, i) { return "translate(-50," + (300 + i * 20) + ")"; });
-  
-    legend.append("rect")
-        .attr("x", width - 19)
-        .attr("width", 19)
-        .attr("height", 19)
-        .attr("fill", z3);
-  
-    legend.append("text")
-        .attr("x", width - 24)
-        .attr("y", 9.5)
-        .attr("dy", "0.32em")
-        .text(function(d) { return d; });
-  });
+      .transition()
+      .duration(500) 
+      .delay(function(d,i){return i*100})     
+      .attr("width", function(d) { return x3(d[0]) - x3(d[1]); })
+      .attr("y", function(d) { return y3(d.data.y); })
+      .attr("x", function(d) { return x3(d[1]); })
+}
+
+});
