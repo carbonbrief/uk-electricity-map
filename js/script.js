26ad65a7bee0453c8775cf7382624573d5c437c2
Rosamund Pearce <rosamundpearce@live.com>
2019-03-19 16:57:22 +0000

    Add placeholder to fix offset :bug:

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const $text= $('.text');
 const $padding = $('.padding');
 const $map = $('#map');
+const $year = $('.year');
 
 let $height = $(window).height();
 let $width = $(window).width();
@@ -29,6 +30,86 @@ $(document).ready(function() {
   window.addEventListener("resize", function(){
       setHeights();
   }, true);
+
+  $year.each(function(){
+
+    let _this = this;
+
+    // travelling up
+    new Waypoint({
+      element: _this,
+      handler: function (direction) {
+          if (direction == 'up') {
+            console.log("up");
+            $year.removeClass("active");
+            $(this.element).addClass("active");
+          }
+      },
+      offset: 45
+    });
+
+    // travelling down
+    new Waypoint({
+      element: _this,
+      handler: function (direction) {
+          if (direction == 'down') {
+            console.log("down");
+            $year.removeClass("active");
+            $(this.element).addClass("active");
+          }
+      },
+      offset: 45
+      // offset: function() {
+      //   return (this.element.clientHeight + 40)
+      // }
+    });
+
+  });
+
+  // different code for the first year to avoid the jump bug
+  new Waypoint({
+    element: $(".year-first"),
+    handler: function (direction) {
+        if (direction == 'down') {
+          console.log("first down");
+          $year.removeClass("active");
+          $(this.element).addClass("active");
+        }
+    },
+    offset: function() {
+      return 40
+    }
+  });
+
+  //different code for the first year to avoid the jump
+  new Waypoint({
+    element: $(".year-first"),
+    handler: function (direction) {
+        if (direction == 'up') {
+          console.log("first up");
+          $year.removeClass("active");
+          $(this.element).removeClass("active");
+        }
+    },
+    offset: function() {
+      return 40
+    }
+  });
+
+});
+
+$(window).on('scroll', function () {
+
+  // let scrollTop = $(this).scrollTop();
+  // let mapTop = $('#main').offset().top;
+
+  // console.log(scrollTop);
+  // console.log(mapTop);
+
+  // if (mapTop < scrollTop) {
+  //   console.log("map below");
+  //   $year.removeClass("active");
+  // };
 
 });
 
