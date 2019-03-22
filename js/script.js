5e07cf30c67b230c7511c2c55465112c645ab759
Rosamund Pearce <rosamundpearce@live.com>
2019-03-22 11:44:36 +0000

    Set heights of year container automatically

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const $text= $('.text');
-const $padding = $('.padding');
+const $paddingBelow = $('.padding-below');
+const $paddingAbove= $('.padding-above');
 const $map = $('#map');
 const $year = $('.year');
 const $yearCont = $('#year-container');
@@ -18,17 +19,32 @@ function setHeights () {
   $map.css("height", $height);
 
   if ($width > 979) {
-    $padding.css("height", $height*0.7);
+    $paddingBelow.css("height", $height*0.7);
   } else {
     // further apart since now covering the map
-    $padding.css("height", $height*0.9);
+    $paddingBelow.css("height", $height*0.9);
   }
+
+  $paddingAbove.css("height", ($(".year-sticky").height()* 3));
 
   $yearCont.css("left", function () {
     return 40 + $barCont.width();
   });
 
   $bar.css("height", ($height - 80));
+
+  // for every year, loop through the text objects with the corresponding index
+  // set height of year container to be equal to this
+  $year.each(function(i, obj) {
+    let outerHeight = 0;
+    $(".text-index" + i).each(function() {
+      outerHeight += $(this).outerHeight();
+    });
+    console.log(outerHeight);
+    $(this).css("height", outerHeight + "px");
+  });
+
+
 
 }
 
