fdac988503634138dda6ac547d1cb3a45c47bda3
Rosamund Pearce <rosamundpearce@live.com>
2019-04-29 12:09:33 +0100

    Account for different margin on tablet view

--- js/script.js
+++ js/script.js
@@ -49,7 +49,12 @@ function setHeights () {
     $yearCont.css("left", function () {
       return getYearPadding() + $barCont.width();
     });
+  } else if ($width < 737 && $width > 480 ) {
+    $yearCont.css("left", function () {
+      return ($width/2 - ($yearCont.width()/2) - 25);
+    });
   } else {
+    // different margin to account for
     $yearCont.css("left", function () {
       return ($width/2 - ($yearCont.width()/2) - 15);
     });
