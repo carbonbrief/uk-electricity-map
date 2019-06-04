51fdb0fc42f963a0e108c3dd3e73afe1e64177d9
Rosamund Pearce <rosamundpearce@live.com>
2019-06-04 15:41:06 +0100

    basic sparkline drawing

--- js/sparkline.js
+++ js/sparkline.js
@@ -2,10 +2,43 @@ const sparkMargin = {top: 5, right: 30, bottom: 5, left: 30},
     sparkWidth = 200 - sparkMargin.left - sparkMargin.right,
     sparkHeight = 50 - sparkMargin.top - sparkMargin.bottom;
 
-const sparkData = [495, 446, 457, 441, 484, 452, 401, 332, 256, 226, 207];
+const sparkData = [{
+        "xVal": 2008,
+        "yVal": 495
+    },{
+        "xVal": 2009,
+        "yVal": 446
+    },{
+        "xVal": 2010,
+        "yVal": 457
+    },{
+        "xVal": 2011,
+        "yVal": 441
+    },{
+        "xVal": 2012,
+        "yVal": 484
+    },{
+        "xVal": 2013,
+        "yVal": 452
+    },{
+        "xVal": 2014,
+        "yVal": 401
+    },{
+        "xVal": 2015,
+        "yVal": 332
+    },{
+        "xVal": 2016,
+        "yVal": 256
+    },{
+        "xVal": 2017,
+        "yVal": 226
+    },{
+        "xVal": 2018,
+        "yVal": 207
+}];
 
 const sparkX = d3.scaleLinear()
-    .domain([0, sparkData.length])
+    .domain([2008, 2018])
     .range([0, sparkWidth]);
 
 const sparkY = d3.scaleLinear()
@@ -13,8 +46,10 @@ const sparkY = d3.scaleLinear()
     .range([sparkHeight, 0]);
 
 const sparkLine = d3.line()
-    .x(function(d, i) { return sparkX(i); })// set the x values for the line generator
-    .y(function(d) { return sparkY(d.y); }) // set the y values for the line generator 
+    .x(function(d) { 
+        return sparkX(d.xVal); 
+    })
+    .y(function(d) { return sparkY(d.yVal); })
     .curve(d3.curveMonotoneX);
 
 const sparkSvg = d3.selectAll(".sparkline").append("svg")
@@ -22,5 +57,10 @@ const sparkSvg = d3.selectAll(".sparkline").append("svg")
     .attr("height", sparkHeight + sparkMargin.top + sparkMargin.bottom)
     .append("g")
     .attr("transform", "translate(" + sparkMargin.left + "," + sparkMargin.top + ")");
+
+sparkSvg.append("path")
+    .datum(sparkData)
+    .attr("class", "sparkPath")
+    .attr("d", sparkLine);
 
 console.log("sparkline");
