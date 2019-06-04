67285a7fdb9bc9357455d6a1dcc9bff3eb6e6a0b
Rosamund Pearce <rosamundpearce@live.com>
2019-06-04 15:20:06 +0100

    ensure names of spark vars unique

--- js/sparkline.js
+++ js/sparkline.js
@@ -1,18 +1,26 @@
-const data = [495, 446, 457, 441, 484, 452, 401, 332, 256, 226, 207];
-
 const sparkMargin = {top: 5, right: 30, bottom: 5, left: 30},
     sparkWidth = 200 - sparkMargin.left - sparkMargin.right,
     sparkHeight = 50 - sparkMargin.top - sparkMargin.bottom;
 
-const xScale = d3.scaleLinear()
-    .domain([0, data.length])
+const sparkData = [495, 446, 457, 441, 484, 452, 401, 332, 256, 226, 207];
+
+const sparkX = d3.scaleLinear()
+    .domain([0, sparkData.length])
     .range([0, sparkWidth]);
 
-const yScale = d3.scaleLinear()
+const sparkY = d3.scaleLinear()
     .domain([200, 500])
-    .range([height, 0]);
+    .range([sparkHeight, 0]);
 
-const line = d3.line()
-    .x(function(d) { return xScale(d.x); }) // set the x values for the line generator
-    .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
+const sparkLine = d3.line()
+    .x(function(d, i) { return sparkX(i); })// set the x values for the line generator
+    .y(function(d) { return sparkY(d.y); }) // set the y values for the line generator 
     .curve(d3.curveMonotoneX);
+
+const sparkSvg = d3.selectAll(".sparkline").append("svg")
+    .attr("width", sparkWidth + sparkMargin.left + sparkMargin.right)
+    .attr("height", sparkHeight + sparkMargin.top + sparkMargin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + sparkMargin.left + "," + sparkMargin.top + ")");
+
+console.log("sparkline");
